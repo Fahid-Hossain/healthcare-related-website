29fddb7eebf718341b512fee804f2e16b5d6f2a9
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-18 03:08:58 +0600

    contact us section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import './App.css';
+import Contact from './components/Homepage/Contact/Contact';
+import Footer from './components/Homepage/Footer/Footer';
 import Header from './components/Homepage/Header/Header';
 import Home from './components/Homepage/Home/Home';
 
@@ -7,11 +9,23 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Route>
+        <Header></Header>
+      </Route>
         <Switch>
-          <Route>
+          <Route exact path="/">
             <Home></Home>
           </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
         </Switch>
+          <Route>
+            <Footer></Footer>
+          </Route>
       </BrowserRouter>
     </div>
   );
